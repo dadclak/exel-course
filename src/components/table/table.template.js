e76a2cff98d072b954dabb9550e1c4da60c1654a
dadclak <kozanchyn.vladeo@gmail.com>
2021-06-02 16:25:19 +0200

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,22 +3,31 @@ const CODES = {
   Z: 90,
 }
 
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${col}" ></div>
   `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index }">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
   `
 }
 
 function createRow(content, rowNumber='') {
+  const resize = rowNumber
+    ? '<div class="row-resize" data-resize="row"></div>'
+    : ''
   return `
-    <div class="row">
-      <div class="row-info">${rowNumber}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${rowNumber}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable guard-for-in */
 class Dom {
   constructor(selector) {
     this.$el = typeof selector === 'string'
@@ -38,6 +40,30 @@ class Dom {
     }
 
     return this
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object
+        .keys(styles)
+        .forEach(key => {
+          this.$el.style[key] = styles[key]
+        })
   }
 }
 
