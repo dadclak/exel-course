778b6ae79d2cdd2e1fb1ea970af32a0dcde5f618
dadclak <kozanchyn.vladeo@gmail.com>
2021-07-12 15:37:52 +0200

    add observer class

---
+++ src/core/Emitter.js
@@ -0,0 +1,23 @@
+export class Emitter {
+  constructor() {
+    this.listeners = []
+  }
+
+  emit(event, ...args) {
+    if (!Array.isArray(this.listeners[event])) {
+      return false
+    }
+    this.listeners[event].forEach(listener => {
+      listener(...args)
+    });
+  }
+
+  subscribe(event, fn) {
+    this.listeners[event] = this.listeners[event] || []
+    this.listeners[event].push(fn)
+    return () => {
+      this.listeners[event] =
+        this.listeners[event].filter(listener => listener !== fn)
+    }
+  }
+}
